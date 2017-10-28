2f93882d8b9c7ba4f207ad25a115ea377f9f8289
Kim Foukaris <kimfoukaris@outlook.com>
2017-10-28 14:55:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
 function appendKitten(name) {
-  var moreKittens = kittens.push(name)
+  var moreKittens = [name, ...kittens]
   return moreKittens
 }
